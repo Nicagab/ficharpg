4aaa3da35daf49997890eba47a2673408488b64d
Gabriela Garcia <nicagabi2@gmail.com>
2026-02-05 23:04:25 -0300

    Alteração na Navbar

--- src/components/layout/navbar/navbar.jsx
+++ src/components/layout/navbar/navbar.jsx
@@ -4,7 +4,16 @@ import Logo from '../../../img/Logo.png'
 function Navbar(){
     return(
         <>
-        <header className={styles.header}><a href="https://discord.gg/leagueofhu3br"><img src={Logo} alt="League of HU3"/></a></header>
+        <header className={styles.header}>
+            <a href="https://discord.gg/leagueofhu3br"><img src={Logo} alt="League of HU3"/></a>
+            <section>
+                <ul>
+                    <li>Fichas</li>
+                    <li>Livro do Jogador</li>
+                    <li>Mesas</li>
+                </ul>
+            </section>
+            </header>
         </>
     )
 }
